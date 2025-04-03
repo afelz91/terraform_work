e350db2375b71517f80fd38ac05e8855ca4c905d
afelz91 <oladapson58@gmail.com>
2025-04-03 11:14:40 +0100

    Initial commit: Terraform setup for AWS VPC and EC2

---
+++ MAIN3.tf
@@ -0,0 +1,78 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  count                   = 3
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.${count.index}.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = element(["us-east-1a", "us-east-1b", "us-east-1c"], count.index)
+}
+
+resource "aws_subnet" "private" {
+  count             = 3
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.${count.index + 3}.0/24"
+  availability_zone = element(["us-east-1a", "us-east-1b", "us-east-1c"], count.index)
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
+resource "aws_route_table_association" "public" {
+  count          = 3
+  subnet_id      = element(aws_subnet.public[*].id, count.index)
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "web" {
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "web_key" {
+  key_name   = "web-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_instance" "web" {
+  ami                         = "ami-0c55b159cbfafe1f0"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public[0].id
+  security_groups             = [aws_security_group.web.name]
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.web_key.key_name
+
+  tags = {
+    Name = "nginx-server"
+  }
+}
+
+
